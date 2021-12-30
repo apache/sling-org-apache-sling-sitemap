3b4f953475363e9fb2cc5f5217f29715a5f8abfa
Dirk Rudolph <diru@apache.org>
2021-12-30 14:38:16 +0100

    trivial: use junit5 in ChainedIteratorTest

--- src/test/java/org/apache/sling/sitemap/impl/helper/ChainedIteratorTest.java
+++ src/test/java/org/apache/sling/sitemap/impl/helper/ChainedIteratorTest.java
@@ -25,10 +25,10 @@ import java.util.NoSuchElementException;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertThrows;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ChainedIteratorTest {
 
